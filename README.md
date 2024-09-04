05e86940c050fe6b88cd09a484c62c007e7ddf4d
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2024-09-04 11:23:00 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Docusaurus PDF
+
+Create a PDF of W&B Docs with [`docs-to-pdf`](https://github.com/jean-humann/docs-to-pdf). 
+
+## Requirements
+
+1. GitHub access and access to W&B GitHub organization.
+2. Satisfy [Docusaurus dependencies](https://github.com/wandb/docodile/tree/main?tab=readme-ov-file#docusaurus-dependencies).
+3. Install `docs-to-pdf`:
+```
+npm install -g docs-to-pdf
+```
+
+## How it works
+The scripts in this repo do a couple of things:
+1. It removes markdown that breaks `docs-to-pdf`
+2. It uses a local host on port `3000` as the staging area where `docs-to-pdf` grabs the docs from and generates PDFs.
+
+For the second point, this means that, if you click on a URL within the PDF, it will redirect you to a path that starts with `http://localhost:3000/`. In other words, you will not be redirected to a public website. This is done on purpose since the primary users of the W&B docs PDF do not have internet restrictions.
+
+## How to use
+
+The default setting of the script generates the Developer guide portion of the docs. In other words, only content that starts with `https://docs.wandb.ai/guides`. To create other parts of the docs, change the `DOC_TYPE` in `make_pdf.sh` to: 
+
+| Docs | DOC_TYPE | 
+| ----- | ----- |
+| [Developer guide](https://docs.wandb.ai/guides) | `guides` | 
+| [Tutorials](https://docs.wandb.ai/tutorials) | `tutorials` | 
+| [API Reference guide](https://docs.wandb.ai/ref) | `ref` |
+
+You can specify a smaller subset of docs by providing the additional path name. For example, if you want to only generate the W&B Python SDK, specify `ref/python` for `DOC_TYPE`.
+
+Once you have the decided which docs you want to generate, run the `make_pdf.sh` script:
+
+```bash
+bash ./scripts/make_pdf.sh
+```
+
---
+++ scripts/make_pdf.sh
@@ -0,0 +1,85 @@
+#!/bin/usr
+
+PORT=3000
+DOC_TYPE="guides"
+initialDocURLs="http://localhost:$PORT/$DOC_TYPE/"
+SCRIPT_DIR="generate_doc_pdf"
+
+# SUBDIR="Desktop/create_doc_pdf"
+# TARGET_PATH="$HOME/$SUBDIR"
+
+# echo $TARGET_PATH
+
+WORKING_DIR=$PWD
+echo $PWD
+
+OUTPUT_DIR="generated_pdf"
+OUTPUT_FILENAME="wb_dev_guide.pdf"
+OUTPUT_PATH="$WORKING_DIR/$OUTPUT_DIR"
+
+
+# Check if output path exists
+if [ -d $OUTPUT_PATH ]; then
+    echo "Output filepath exists...skipping making output directory"
+else
+    echo "Creating output filepath..."
+    mkdir $OUTPUT_PATH
+fi
+
+# Find the PID of the process using the specified port
+PID=$(lsof -t -i:$PORT)
+
+# Check if a PID was found
+if [ -n "$PID" ]; then
+    echo "Port $PORT is in use by process $PID. Killing it..."
+    
+    # Kill the process
+    kill -9 "$PID"
+    
+    # Check if the process was killed successfully
+    if [ $? -eq 0 ]; then
+        echo "Process $PID killed successfully."
+    else
+        echo "Failed to kill process $PID."
+        exit 1
+    fi
+else
+    echo "Port $PORT is not in use."
+fi
+
+# Check if repo exists
+if [ -d "$WORKING_DIR/docodile" ]; then
+  echo "GitHub repo exists..."
+  cd docodile
+  echo "Checkout out main..."
+  git checkout main
+  git pull
+else
+    git clone https://github.com/wandb/docodile.git
+    cd docodile
+fi
+
+# Install React app
+yarn install 
+
+# Start local server and give server some time before it handles requests
+yarn start &  
+sleep 10 
+
+# Copy processing script to docodile
+cp "$WORKING_DIR/$SCRIPT_DIR/removeTags.js" "$WORKING_DIR/docodile/scripts/"
+
+# Remove accordian tags because this breaks PDF generator
+node ./scripts/removeTags.js 
+
+# Create PDF with docs-to-pdf
+npx docs-to-pdf --initialDocURLs=$initialDocURLs \
+    --contentSelector="article" \
+    --paginationSelector="a.pagination-nav__link.pagination-nav__link--next" \
+    --excludeSelectors=".margin-vert--xl a,[class^='tocCollapsible'],.breadcrumbs,.theme-edit-this-page" \
+    --protocolTimeout="7200000" \
+    --coverSub="Documentation" \
+    --coverTitle="Weights & Biases" \
+    --outputPDFFilename="$OUTPUT_PATH/$OUTPUT_FILENAME"
+    # --coverImage="https://docusaurus.io/img/docusaurus.png" \
+
